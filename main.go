6950ac7a25fe2ce7232a26fe70993d29b9dadd88
jan <jan.reyes.contact@gmail.com>
2024-09-24 22:27:39 -0600

    feat: created basic http server

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
+
+func newMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", greet)
+
+	return mux
+}
+
 func main() {
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: newMux(),
+	}
+	log.Fatal(server.ListenAndServe())
 }
